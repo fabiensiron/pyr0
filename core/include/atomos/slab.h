d8bb40b2a88fb56c7273a669e030899b1b7065c6
Fabien Siron <fabien.siron@epita.fr>
2016-03-06 00:10:34 +0000

    add vmm/slab headers

---
+++ core/include/atomos/slab.h
@@ -0,0 +1,43 @@
+/*
+ * File: slab.h
+ * Author: Fabien Siron <fabien.siron@epita.fr>
+ *
+ * Description: slab allocator
+ */
+
+#ifndef _SLAB_H_
+# define _SLAB_H_
+
+# include <atomos/vmm.h>
+# include <atomos/kernel.h>
+
+struct slab_cache;
+struct slab;
+struct slab_free_object;
+
+struct slab_cache *
+slab_cache_setup_prepare(u32 kernel_base,
+			 u32 kernel_top,
+			 size_t sizeof_struct_range,
+			 struct slab **first_struct_slab_of_caches,
+			 u32 *first_slab_of_caches_base,
+			 u32 *first_slab_of_caches_nb_pages,
+			 struct slab **first_struct_slab_of_ranges,
+			 u32 *first_slab_of_ranges_base,
+			 u32 *first_slab_of_ranges_nb_pages);
+int slab_cache_setup_commit(struct slab *first_struct_slab_of_caches,
+			struct vmm_range *first_range_of_caches,
+			struct slab *first_struct_slab_of_ranges,
+  			struct vmm_range *first_range_of_ranges);
+struct slab_caches *slab_cache_create(const char *name,
+				      size_t obj_size,
+				      u32 pages_per_slab,
+				      u32 min_free_objs,
+				      u32 cache_flags);
+int slab_cache_destroy(struct slab_cache *slab_cache);
+u32 slab_cache_alloc(struct slab_cache *slab_cache, u32 alloc_flags);
+u32 slab_cache_free(u32 vaddr);
+struct vmm_range **
+slab_cache_release_struct_range(struct vmm_range *the_range);
+
+#endif /*!_SLAB_H_*/
---
+++ core/include/atomos/vmm.h
@@ -0,0 +1,24 @@
+/*
+ * File: vmm.h
+ * Author: Fabien Siron <fabien.siron@epita.fr>
+ *
+ * Description: first fit memory allocator to manage vmm
+ */
+#ifndef _VMM_H_
+# define _VMM_H_
+
+# include <atomos/slab.h>
+# include <atomos/kernel.h>
+
+struct vmm_range;
+
+int vmm_setup(u32 kern_base, u32 kern_top, u32 stack_bottom, u32 stack_top);
+struct vmm_range *vmm_new_range(u32 nb_pages, u32 flags, u32 *range_start);
+int vmm_del_range(struct vmm_range *range);
+u32 vmm_alloc(u32 nb_pages, u32 flags);
+int vmm_free(u32 vaddr);
+int vmm_set_slab(struct vmm_range *range, struct slab *slab);
+struct slab*vmm_resolve_slab(u32 vaddr);
+int vmm_is_valid_vaddr(u32 vaddr);
+
+#endif /*!_VMM_H_*/
