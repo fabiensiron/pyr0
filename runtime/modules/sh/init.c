0ad473b9c3756268d07077622291ff37f97b343e
Fabien Siron <fabien.siron@epita.fr>
2016-05-22 15:47:02 +0000

    runtime/modules: add classical python __doc__ to sh module

--- runtime/modules/sh/init.c
+++ runtime/modules/sh/init.c
@@ -11,6 +11,13 @@
 extern tp_obj sh_echo(TP);
 extern tp_obj sh_yes(TP);
 
+static char *help =
+	"This module rules the pyr0's shell.\n"
+	"\n"
+	"This exports:\n"
+	"\t* sh.echo echoes the user argument\n"
+	"\t* sh.yes prints indefinitely \"yes\" or the given argument\n";
+
 void sh_init(TP)
 {
   /* module */
@@ -22,7 +29,7 @@ void sh_init(TP)
 
   /* special attributes */
   tp_set(tp, sh_mod, tp_string("__doc__"),
-	 tp_string("This module rules the pyr0s shell."));
+	 tp_string(help));
   tp_set(tp, sh_mod, tp_string("__name__"), tp_string("sh"));
   tp_set(tp, sh_mod, tp_string("__file__"), tp_string(__FILE__));
 
