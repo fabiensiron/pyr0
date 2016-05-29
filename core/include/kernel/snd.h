5e1ad9114b3f56dfcff923f9bb320cb5eef6009e
Fabien Siron <fabien.siron@epita.fr>
2016-05-29 14:50:29 +0000

    core: introduce snd.c

---
+++ core/include/kernel/snd.h
@@ -0,0 +1,11 @@
+/**
+ * File: snd.h
+ * Author: Fabien Siron <fabien.siron@epita.fr> 
+ */
+
+#ifndef _SND_H_
+# define _SND_H_
+
+void beep(void);
+
+#endif /*_SND_H_*/
---
+++ core/kernel/snd.c
@@ -0,0 +1,40 @@
+/*
+ * File: snd.c
+ * Author: Fabien Siron, From osdev
+ */
+
+#include <kernel/pyr0.h>
+#include <kernel/pit.h>
+
+#include <asm/io.h>
+
+static void play_sound(u32 nFrequence) {
+	u32 Div;
+	u8 tmp;
+
+	//Set the PIT to the desired frequency
+	Div = 1193180 / nFrequence;
+	outb(0x43, 0xb6);
+	outb(0x42, (u8) (Div) );
+	outb(0x42, (u8) (Div >> 8));
+
+	//And play the sound using the PC speaker
+	tmp = inb(0x61);
+	if (tmp != (tmp | 3)) {
+		outb(0x61, tmp | 3);
+	}
+}
+
+//make it shutup
+static void nosound() {
+	u8 tmp = inb(0x61) & 0xFC;
+
+	outb(0x61, tmp);
+}
+
+//Make a beep
+void beep() {
+	play_sound(1000);
+	pit_wait(10);
+	nosound();
+}
